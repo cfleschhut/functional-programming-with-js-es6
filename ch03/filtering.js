2796ad9275123964b7be3ceac4447a88005dfbf8
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-07-06 23:58:19 +0200

    Add filter example

---
+++ ch03/filtering.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const words = ['lorem', 'ipsum', 'dolor', 'sit', 'amet'];
+
+const evenNumbers = numbers.filter((x) => x % 2 === 0);
+console.log(evenNumbers);
+
+const createLengthTest = (minLength) => (word) => word.length > minLength;
+
+const longWords = words.filter(createLengthTest(4));
+console.log(longWords);
