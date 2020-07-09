b4bd332d9089bc8851c69fdbdf5c14df93e17f01
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-07-09 23:24:12 +0200

    Add test case for multiple occurrences of letters in a word

--- src/letter-count.test.js
+++ src/letter-count.test.js
@@ -15,4 +15,16 @@ describe('getLetterCount', () => {
 
     expect(actual).to.deep.equal(expected);
   });
+
+  it('returns the correct letter count for words with more than one of certain letters', () => {
+    const expected = {
+      m: 1,
+      i: 4,
+      s: 4,
+      p: 2,
+    };
+    const actual = getLetterCount('mississippi');
+
+    expect(actual).to.deep.equal(expected);
+  });
 });
